cfe404bf773c6506e72e588cf1d9770d6b57a95f
somesh soni <someshdevops89@gmail.com>
2025-08-20 18:35:12 +0530

    new features added

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 var change = document.querySelector("h5")
 var target = document.querySelector("#Friend")
-var removeFriend = document.querySelector("#remove")
+flag = 0;
+
 target.addEventListener("click",function(){
+    if(flag == 0){
 
     change.innerHTML = "Friends"
     change.style.color = "green"
+    target.innerHTML = "Unfollow"
+    flag = 1
 
+}else{
+    change.innerHTML = "Suggestion"
+    change.style.color = "palevioletred"
+    target.innerHTML = "Follow"
 
-
-})
-removeFriend.addEventListener("click", function(){
-
-
-change.innerHTML = "Suggestion"
-change.style.color = "palevioletred"
 
 
 
